e1498131436e88438c4aca479ec0e4c2a214df6e
Sean Sellek <me@seansellek.com>
2015-06-29 15:26:11 -0400

    Notify user when incorrect input is entered

--- controller.rb
+++ controller.rb
@@ -54,6 +54,9 @@ class Controller
         @state = :help
       when /^exit$/i
         @running = false
+      else
+        puts "'#{input}' is invalid input."
+        sleep(1)
     end
   end
 
